e75f5ae5feec11b6132fa104b69a2f41b32bc25f
Yulong Tan <swifties@uw.edu>
2016-10-10 20:05:47 -0700

    parsed through text file

--- riddles.php
+++ riddles.php
@@ -1,5 +1,22 @@
 <?php
 
-
+$riddle_file = "riddles.txt";
+$riddles = explode("\n", file_get_contents($riddle_file));
+$data;
+$riddle_count = 0;
+$answer_count = 0;
+$riddle_number = 1;
+foreach ($riddles as $riddle) {
+	$data[] = array {
+		"riddle" => $riddle[$riddle_count],
+		$riddle_count += 2;
+		"answer" => $riddle[$answer_count];
+		$answer_count += 2;
+		"number" => $riddle_number;
+		$riddle_number++;
+	}
+}
+header("Content-type: application/json");
+print json_encode($data);
 
 ?>
