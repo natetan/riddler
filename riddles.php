473eb15214a8875b29def0c9d2383dc509d77299
Yulong Tan <swifties@uw.edu>
2016-10-10 20:10:43 -0700

    fix for loop bug

--- riddles.php
+++ riddles.php
@@ -7,14 +7,15 @@ $riddle_count = 0;
 $answer_count = 0;
 $riddle_number = 1;
 foreach ($riddles as $riddle) {
-	$data[] = array {
+	$data[] = array (
 		"riddle" => $riddle[$riddle_count],
-		$riddle_count += 2;
-		"answer" => $riddle[$answer_count];
-		$answer_count += 2;
-		"number" => $riddle_number;
-		$riddle_number++;
-	}
+		"answer" => $riddle[$answer_count],
+		"number" => $riddle_number,
+		
+	);
+	$riddle_count += 2;
+	$answer_count += 2;
+	$riddle_number++;
 }
 header("Content-type: application/json");
 print json_encode($data);
