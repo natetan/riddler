bfb02ec58821e7a0c3e70057b65b1af9d631b05a
Yulong Tan <yulongtan67@gmail.com>
2016-11-25 01:51:30 -0800

    added the rest of the code in jquery format

--- scripts/jq-riddler.js
+++ scripts/jq-riddler.js
@@ -1,19 +1,53 @@
 $(document).ready(function() {
 
+	var riddles = new Array();
+	var currentRiddle;
+
 	window.onload = function() {
-		makeAjaxRequest("riddles.php", storeRiddles);
+		makeAjaxRequest('riddles.php');
 		$('#answer-area').html('');
 		$('#answer').on('click', showAnswer);
 		$('#next').on('click', getNextRiddle);
 		$('#start').on('click', start);
 	};
 
-	function makeAjaxRequest(url, methodName) {
+	function makeAjaxRequest(url) {
 		$.ajax({
-			url: 'riddles.php',
+			url: url,
+			type: 'GET',
 			context: document.body,
-			success: methodName
+			dataType: 'json',
+			success: function() {
+				var data = $.parseJSON(this.responseText);
+				for (var i = 0; i < data.length; i++) {
+					riddles[i] = new riddle(data[i].riddle, data[i].answer, data[i].number );
+				}
+			}
 		});
+	}
+
+	function riddle(riddle, answer, number) {
+		this.riddle = riddle;
+		this.answer = answer;
+		this.number = number;
+	}
+
+	function getRandomRiddle() {
+		var random = Math.floor(Math.random() * (riddles.length));
+		return riddles[random];
+	}
+
+	function getNextRiddle() {
+		currentRiddle = getRandomRiddle();
+		$('.riddle-number').html('Riddle #' + currentRiddle.number);
+		$('#riddle').html('currentRiddle.riddle');
+		$('.answer-area').addClass('hide');
+	}
+
+	function showAnswer() {
+		var answer = $('.answer-area');
+		answer.html(currentRiddle.answer);
+		answer.removeClass('hide');
 	}
 	
 });
